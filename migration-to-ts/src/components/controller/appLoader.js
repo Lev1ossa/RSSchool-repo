82e250b0b0fe3829e7d3caf6c028b5be4c9c3d0b
Lev1ossa <ikehhui@gmail.com>
2023-06-12 00:38:28 +0600

    feat: add api key

--- migration-to-ts/src/components/controller/appLoader.js
+++ migration-to-ts/src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '1b6714bc3f3c48d6ac3239ae1f78c017', // получите свой ключ https://newsapi.org/
         });
     }
 }
